57d002845f16d50a9930d39ffd1ab701f82c2461
Miangaly <miangaly.randriamifidy@gmail.com>
2018-12-06 20:41:17 +0400

    first commit

---
+++ Book/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Book.Models;
+
+namespace Book.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            var db = new BookContext();
+            var utilisateurs = from u in db.Utilisateurs
+                        orderby u.Nom
+                        select u;
+            return View(utilisateurs);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Book/Models/Utilisateur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Book.Models
+{
+    public class Utilisateur
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+    }
+
+    public class BookContext : DbContext
+    {
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
+    }
+}
